2322d374bd5fd695d5435a65ae95d8a100a1245e
Charlotte Skinner <cs.skinner90@hotmail.co.uk>
2022-12-19 07:29:04 +0000

    fix dark mode posts

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,7 +6,7 @@ const Intro = () => {
 
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="dark:text-white text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="dark:text-white text-6xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8 text-center md:text-left">
         Charlotte Skinner
       </h1>
       <div className="flex-col md:flex-row flex items-center md:justify-between">
